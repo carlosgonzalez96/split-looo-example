16043445cb347d9c5a88dd80ad071e1f34a8cbcc
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-20 20:08:28 -0700

    remove comment

--- refactoring1.py
+++ refactoring1.py
@@ -1,6 +1,5 @@
 import json
 
-# length: cm, venom: mg
 scorpions =  '{ "length":[0.9,21,1,10,21,5,18,4,15,2], "venom":[0.39,0.62,0.20,0.45,0.33,0.62,0.55,0.40,0.22,0.38]}'
 scorpion_data = json.loads(scorpions)
 
