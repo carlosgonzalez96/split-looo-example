6328ee9b3ea8b9f67b7bd6843c49718d801c5871
carlosgonzalez96 <carlosgonz817@gmail.com>
2021-05-26 18:15:33 -0700

    added tutorial pdf

--- refactoring1.py
+++ refactoring1.py
@@ -1,17 +1,17 @@
+
+
 import json
 
 scorpions =  '{ "length":[0.9,21,1,10,21,5,18,4,15,2], "venom":[0.39,0.62,0.20,0.45,0.33,0.62,0.55,0.40,0.22,0.38]}'
 scorpion_data = json.loads(scorpions)
 
-total_venom_amount = 0
-average_length = 0
+def average_scorpion_length():
+    average_length = reduce(lambda total,data: data+total, scorpion_data["length"])
+    return average_length
 
-for data in range(10):
-    average_length += scorpion_data["length"][data]
-    if scorpion_data["venom"][data] > 0.40:
-        total_venom_amount += scorpion_data["venom"][data]
+def total_venom_extract():
+    total_venom_amount = sum([i for i in scorpion_data["venom"] if i > 0.40])
+    return total_venom_amount
 
-average_length = average_length / len(scorpion_data["length"])
-
-print "Average length: " + str(average_length) + "cm"
-print "Total amount of venom extracted: " + str(total_venom_amount) + "mg"
+print "Average length: " + str(average_scorpion_length()) + "cm" + "\n" + "Total amount of venom extracted: " + \
+    str(total_venom_extract()) + "mg"
